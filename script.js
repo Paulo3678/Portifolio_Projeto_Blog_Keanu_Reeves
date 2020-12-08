77c9849f285e21f6a7fedab160eda55cc9e06fd6
Paulo Silva <ph7811001@gmail.com>
2020-12-08 09:58:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function animaLetras(){
+  document.getElementById('ke').style.marginLeft = 65 + 'px';
+  document.getElementById('reeves').style.marginLeft = 35 + 'px';
+ 
+}
+
+function desanimaLetras(){
+  document.getElementById('ke').style.marginLeft = 0 + 'px';
+  document.getElementById('reeves').style.marginLeft = 0 + 'px';
+}
